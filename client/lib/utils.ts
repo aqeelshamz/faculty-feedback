3b127c6e29c6ea8c6cf95e6a597588834f6a7aaf
Aqeel <aqeelten@gmail.com>
2024-03-24 10:19:07 +0530

    utils

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const serverURL = "http://localhost:8080";
+
+export { serverURL };
