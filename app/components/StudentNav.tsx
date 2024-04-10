0fe3d2fd8257098b2b3bf7963303cde9a2220f73
Aqeel <aqeelten@gmail.com>
2024-04-10 21:16:58 +0530

    student navbar update

--- app/components/StudentNav.tsx
+++ app/components/StudentNav.tsx
@@ -15,7 +15,7 @@ export const StudentNav = () => {
             </Link>
             <Link
                 href="/dashboard/feedbacks"
-                className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${pathname === "/dashboard/programs" ? "bg-muted text-primary" : "text-muted-foreground"}`}
+                className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary ${pathname === "/dashboard/feedbacks" ? "bg-muted text-primary" : "text-muted-foreground"}`}
             >
                 <GraduationCap className="h-4 w-4" />
                 Feedbacks
