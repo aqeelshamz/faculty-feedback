f4391e336b0c884935b159191e63c3dcf6dd92b2
Aqeel <aqeelten@gmail.com>
2024-04-10 22:07:31 +0530

    utils: feedbackFormColors

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,5 +5,24 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
 
+const feedbackFormColors = {
+    "red": {
+        "light": "bg-red-50",
+        "dark": "bg-red-700",
+    },
+    "yellow": {
+        "light": "bg-yellow-50",
+        "dark": "bg-yellow-700",
+    },
+    "green": {
+        "light": "bg-green-50",
+        "dark": "bg-green-700",
+    },
+    "blue": {
+        "light": "bg-blue-50",
+        "dark": "bg-blue-700",
+    },
+};
+
 const serverURL = process.env.NEXT_PUBLIC_SERVER_URL; // https://campusapi.vercel.app
-export { serverURL };
+export { serverURL, feedbackFormColors };
