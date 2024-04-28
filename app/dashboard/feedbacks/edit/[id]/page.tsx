0b7d68c4b275987aeae0e310500b74674aa4c335
sai-na <saipkmm100@gmail.com>
2024-04-28 15:37:24 +0530

    feedback page update

--- app/dashboard/feedbacks/edit/[id]/page.tsx
+++ app/dashboard/feedbacks/edit/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
+import { CirclePicker } from "react-color";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -67,7 +68,7 @@ export default function EditFeedback() {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [color, setColor] = useState("");
+    const [color, setColor] = useState("#EC7777");
     const [course, setCourse] = useState("");
     const [questions, setQuestions] = useState<Question[]>([initialQuestion]);
     const [courses, setCourses] = useState<any>([]);
@@ -175,6 +176,10 @@ export default function EditFeedback() {
         getCourses();
     }, [id]);
 
+    const handleColorChange = (newColor: { hex: SetStateAction<string> }) => {
+        setColor(newColor.hex);
+    };
+
     // function handleInputChange(index: number, event: React.ChangeEvent<HTMLInputElement>) {
     //     const { value } = event.target;
     //     setQuestions((prevQuestions: Question[]) => {
@@ -252,10 +257,10 @@ export default function EditFeedback() {
 
     function TextQuestionTemplate({ questionNumber }: { questionNumber: number }) {
         return (
-            <div className="flex flex-col space-y-4">
+            <div className="flex w-full flex-col space-y-4">
                 <div className="flex items-center space-x-2">
                     <p className="text-lg">{questionNumber + "."}</p>
-                    <Input placeholder="Question" size={100} />
+                    <Textarea placeholder="Question" className="w-full" />
                 </div>
             </div>
         );
@@ -263,10 +268,10 @@ export default function EditFeedback() {
 
     function RatingQuestionTemplate({ questionNumber }: { questionNumber: number }) {
         return (
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col w-full space-y-4">
                 <div className="flex items-center space-x-2">
                     <p className="text-lg">{questionNumber + "."}</p>
-                    <Input placeholder="Question" size={100} />
+                    <Textarea placeholder="Question" className="w-full" />
                 </div>
                 <div className="space-y-4 py-4">
                     <Slider defaultValue={[0]} max={5} step={1} />
@@ -277,15 +282,10 @@ export default function EditFeedback() {
 
     function LongTextQuestionTemplate({ questionNumber }: { questionNumber: number }) {
         return (
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col  w-full space-y-4">
                 <div className="flex items-center space-x-2">
                     <p className="text-lg">{questionNumber + "."}</p>
-                    <Input
-                        type="text"
-                        placeholder="Question"
-                        size={100}
-                        // onChange={(event) => handleInputChange(questionNumber - 1, event)}
-                    />
+                    <Textarea placeholder="Question" className="w-full" />
                 </div>
             </div>
         );
@@ -299,12 +299,13 @@ export default function EditFeedback() {
         options: string[];
     }) {
         return (
-            <div className="flex flex-col space-y-4">
+            <div className="flex flex-col w-full space-y-4">
                 <div className="flex items-center space-x-2">
                     <p className="text-lg">{questionNumber + "."}</p>
-                    <Input placeholder="Question" />
+
+                    <Textarea placeholder="Question" className="w-[90%]" />
                 </div>
-                <div className="space-y-2">
+                <div className="space-y-2 ml-5">
                     {options?.map((_, index) => (
                         <div key={index} className="flex space-y-2 items-center space-x-2">
                             <Label>{String.fromCharCode(65 + index) + "."}</Label>
@@ -336,28 +337,28 @@ export default function EditFeedback() {
     }
 
     return (
-        <div className="p-4 w-full space-y-8 overflow-y-auto">
+        <div className="p-4 w-full space-y-8 overflow-y-auto mb-48">
             <div className="flex flex-col gap-y-3">
                 <div className="flex flex-col justify-between space-y-4">
                     <div className="flex justify-between">
                         <Input
-                            className="text-2xl font-medium w-[50%] h-15"
+                            className="text-2xl font-medium max-w-screen-lg h-15"
                             placeholder="Title"
                             value={title}
                             onChange={(e) => setTitle(e.target.value)}
                         />
-                        <Button className="w-24" onClick={() => updateFeedback(id)}>
+                        <Button className="w-48" onClick={() => updateFeedback(id)}>
                             Save
                         </Button>
                     </div>
                     <Textarea
                         placeholder="Description"
-                        className="w-[50%]"
+                        className="max-w-screen-lg"
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                     />
                     <div className="flex flex-col justify-between space-y-4">
-                        <div className="flex justfy-between gap-2">
+                        <div className="flex item-center gap-2">
                             <Select onValueChange={setCourse}>
                                 <SelectTrigger className="w-[180px]">
                                     <SelectValue placeholder="Course" />
@@ -375,30 +376,29 @@ export default function EditFeedback() {
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
-                            {["black", "black", "black", "black", "black"].map((color) => {
-                                return (
-                                    <span
-                                        key={color}
-                                        className={`flex h-6 w-6 items-center justify-center rounded-full bg-${color} bg-`}
-                                    >
-                                        {isActive && <CheckIcon className="h-4 w-4 text-white" />}
-                                    </span>
-                                );
-                            })}
+
+                            <CirclePicker
+                                onChange={handleColorChange}
+                                className="mt-1"
+                                circleSize={28}
+                                color={color}
+                                colors={[
+                                    "#EC7777",
+                                    "#AAEC77",
+                                    "#77ECC2",
+                                    "#779FEC",
+                                    "#EC77BD",
+                                    "#ECA177",
+                                ]}
+                            />
                         </div>
                     </div>
-                </div>
-                <div className="flex justify-between">
-                    <Button onClick={() => setQuestions([...questions, initialQuestion])}>
-                        <Plus className="mr-2" size={20} />
-                        New question
-                    </Button>
                 </div>
             </div>
             {questions?.map((question, index) => {
                 if (question.questionType === "mcq") {
                     return (
-                        <Card key={index} className="w-[700px] my-2 py-6 ">
+                        <Card key={index} className="max-w-screen-lg my-2 py-6 ">
                             <CardContent>
                                 <div className="flex flex-row space-x-4 justify-between">
                                     <MultipleChoiceQuestionTemplate
@@ -413,9 +413,9 @@ export default function EditFeedback() {
                                     </div>
                                 </div>
                             </CardContent>
-                            <CardFooter className="ml-5">
+                            <CardFooter className="ml-10">
                                 <Button
-                                    size={"sm"}
+                                    className="lg:w-[70%] md:w-[60%] w-[80%]"
                                     onClick={() => {
                                         const newOptions = [...question.options, { option: "" }];
                                         setQuestions((prevQuestions: Question[]) => {
@@ -436,7 +436,7 @@ export default function EditFeedback() {
                     );
                 } else if (question.questionType === "rating") {
                     return (
-                        <Card key={index} className="w-[700px] my-2 pt-6 ">
+                        <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
                             <CardContent>
                                 <div className="flex flex-row space-x-4 justify-between">
                                     <RatingQuestionTemplate questionNumber={index + 1} />
@@ -452,7 +452,7 @@ export default function EditFeedback() {
                     );
                 } else if (question.questionType === "longtext") {
                     return (
-                        <Card key={index} className="w-[700px] my-2 pt-6 ">
+                        <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
                             <CardContent>
                                 <div className="flex flex-row space-x-4 justify-between">
                                     <LongTextQuestionTemplate questionNumber={index + 1} />
@@ -468,7 +468,7 @@ export default function EditFeedback() {
                     );
                 } else {
                     return (
-                        <Card key={index} className="w-[700px] my-2 pt-6 ">
+                        <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
                             <CardContent>
                                 <div className="flex flex-row space-x-4 justify-between">
                                     <TextQuestionTemplate questionNumber={index + 1} />
@@ -484,6 +484,15 @@ export default function EditFeedback() {
                     );
                 }
             })}
+
+            <Button
+                className="max-w-screen-lg w-full"
+                onClick={() => setQuestions([...questions, initialQuestion])}
+            >
+                <Plus className="mr-2" size={20} />
+                New question
+            </Button>
+
             <Toaster />
         </div>
     );
