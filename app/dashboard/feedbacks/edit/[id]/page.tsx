4bd5be1653b17312f1741f018d43e270bdcbdb4e
Aqeel <aqeelten@gmail.com>
2024-04-29 12:45:36 +0530

    feedback/edit: render questions init

--- app/dashboard/feedbacks/edit/[id]/page.tsx
+++ app/dashboard/feedbacks/edit/[id]/page.tsx
@@ -208,162 +208,6 @@ export default function EditFeedback() {
         setColor(newColor.hex);
     };
 
-    // function handleInputChange(index: number, event: React.ChangeEvent<HTMLInputElement>) {
-    //     const { value } = event.target;
-    //     setQuestions((prevQuestions: Question[]) => {
-    //         const newQuestions = [...prevQuestions];
-    //         const currentQuestion = newQuestions[index];
-    //         currentQuestion.question = value;
-    //         return newQuestions;
-    //     });
-    // }
-
-    function SelectQuestionType({ index }: { index: number }) {
-        const questionTypes = ["Text", "Multiple Choice", "Rating", "Long Text"];
-
-        const handleOnValueChange = (questionType: string) => {
-            if (questionType === "mcq") {
-                setQuestions((prevQuestions: any) => {
-                    const newQuestions = [...prevQuestions];
-                    newQuestions[index] = {
-                        questionType: "mcq",
-                        question: "",
-                        options: [{ option: "" }, { option: "" }, { option: "" }, { option: "" }],
-                    };
-                    return newQuestions;
-                });
-            } else if (questionType === "rating") {
-                setQuestions((prevQuestions: any) => {
-                    const newQuestions = [...prevQuestions];
-                    newQuestions[index] = { questionType: "rating", question: "", rating: 0 };
-                    return newQuestions;
-                });
-            } else if (questionType === "longtext") {
-                setQuestions((prevQuestions: any) => {
-                    const newQuestions = [...prevQuestions];
-                    newQuestions[index] = { questionType: "longtext", question: "" };
-                    return newQuestions;
-                });
-            } else {
-                setQuestions((prevQuestions: any) => {
-                    const newQuestions = [...prevQuestions];
-                    newQuestions[index] = { questionType: "text", question: "" };
-                    return newQuestions;
-                });
-            }
-        };
-
-        return (
-            <Select onValueChange={handleOnValueChange} value={questions[index].questionType}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select an option" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>Select an option</SelectLabel>
-                        {questionTypes?.map((qstnType, index) => (
-                            <SelectItem
-                                key={index}
-                                value={
-                                    qstnType === "Multiple Choice"
-                                        ? "mcq"
-                                        : qstnType === "Rating"
-                                          ? "rating"
-                                          : qstnType === "Long Text"
-                                            ? "longtext"
-                                            : "text"
-                                }
-                            >
-                                {qstnType}
-                            </SelectItem>
-                        ))}
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-        );
-    }
-
-    function TextQuestionTemplate({ questionNumber }: { questionNumber: number }) {
-        return (
-            <div className="flex w-full flex-col space-y-4">
-                <div className="flex items-center space-x-2">
-                    <p className="text-lg">{questionNumber + "."}</p>
-                    <Textarea placeholder="Question" className="w-full" />
-                </div>
-            </div>
-        );
-    }
-
-    function RatingQuestionTemplate({ questionNumber }: { questionNumber: number }) {
-        return (
-            <div className="flex flex-col w-full space-y-4">
-                <div className="flex items-center space-x-2">
-                    <p className="text-lg">{questionNumber + "."}</p>
-                    <Textarea placeholder="Question" className="w-full" />
-                </div>
-                <div className="space-y-4 py-4">
-                    <Slider defaultValue={[0]} max={5} step={1} />
-                </div>
-            </div>
-        );
-    }
-
-    function LongTextQuestionTemplate({ questionNumber }: { questionNumber: number }) {
-        return (
-            <div className="flex flex-col  w-full space-y-4">
-                <div className="flex items-center space-x-2">
-                    <p className="text-lg">{questionNumber + "."}</p>
-                    <Textarea placeholder="Question" className="w-full" />
-                </div>
-            </div>
-        );
-    }
-
-    function MultipleChoiceQuestionTemplate({
-        questionNumber,
-        options,
-    }: {
-        questionNumber: number;
-        options: string[];
-    }) {
-        return (
-            <div className="flex flex-col w-full space-y-4">
-                <div className="flex items-center space-x-2">
-                    <p className="text-lg">{questionNumber + "."}</p>
-
-                    <Textarea placeholder="Question" className="w-[90%]" />
-                </div>
-                <div className="space-y-2 ml-5">
-                    {options?.map((_, index) => (
-                        <div key={index} className="flex space-y-2 items-center space-x-2">
-                            <Label>{String.fromCharCode(65 + index) + "."}</Label>
-                            <Input placeholder={`Option ${index + 1}`} size={100} />
-                            <Button
-                                size={"sm"}
-                                variant={"ghost"}
-                                onClick={() => {
-                                    const newOptions = options.filter((_, i) => i !== index);
-                                    setQuestions((prevQuestions: any) => {
-                                        const newQuestions = [...prevQuestions];
-                                        const currentQuestion = newQuestions[questionNumber - 1];
-                                        if (currentQuestion.questionType === "mcq") {
-                                            currentQuestion.options = newOptions?.map((option) => ({
-                                                option,
-                                            }));
-                                        }
-                                        return newQuestions;
-                                    });
-                                }}
-                            >
-                                <Trash2 size={16} />
-                            </Button>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="p-4 w-full space-y-8 overflow-y-auto mb-48">
             <div className="flex flex-col gap-y-3">
@@ -425,56 +269,41 @@ export default function EditFeedback() {
                 </div>
             </div>
             {questions?.map((question: any, index: number) => {
-                return question?.settings?.type === "multiplechoice" ? (
-                    <Card key={index} className="max-w-screen-lg my-2 py-6 ">
-                        <CardContent></CardContent>
-                        <CardFooter className="ml-10"></CardFooter>
-                    </Card>
-                ) : question?.settings?.type === "rating" ? (
-                    <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
-                        <CardContent>
-                            <div className="flex flex-row space-x-4 justify-between">
-                                <RatingQuestionTemplate questionNumber={index + 1} />
-                                <div className="flex flex-row space-x-4">
-                                    <SelectQuestionType index={index} />
-                                    <Button size={"default"} variant={"destructive"}>
-                                        Delete
-                                    </Button>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                ) : question?.settings?.type === "longtext" ? (
-                    <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
-                        <CardContent>
-                            <div className="flex flex-row space-x-4 justify-between">
-                                <LongTextQuestionTemplate questionNumber={index + 1} />
-                                <div className="flex flex-row space-x-4">
-                                    <SelectQuestionType index={index} />
-                                    <Button size={"default"} variant={"destructive"}>
-                                        Delete
-                                    </Button>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                ) : (
-                    <Card key={index} className="max-w-screen-lg my-2 pt-6 ">
-                        <CardContent>
-                            <div className="flex flex-row space-x-4 justify-between">
-                                <TextQuestionTemplate questionNumber={index + 1} />
-                                <div className="flex flex-row space-x-4">
-                                    <SelectQuestionType index={index} />
-                                    <Button size={"default"} variant={"destructive"}>
-                                        Delete
-                                    </Button>
-                                </div>
-                            </div>
-                        </CardContent>
-                    </Card>
-                );
+                return <Card key={index} className="max-w-screen-lg my-2 py-6 ">
+                    <CardContent>
+                        <Button variant={"destructive"} onClick={() => {
+                            setQuestions(questions.filter((_: any, i: any) => i !== index))
+                        }}><Trash2 /></Button>
+                        <Select onValueChange={(x)=>{
+                            question.settings.type = x;
+                            setQuestions([...questions]);
+                        }} value={question.settings.type}>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Course" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Question Type</SelectLabel>
+                                    <SelectItem key={index} value={"text"}>
+                                        Text
+                                    </SelectItem>
+                                    <SelectItem key={index} value={"longtext"}>
+                                        Long Text
+                                    </SelectItem>
+                                    <SelectItem key={index} value={"multiplechoice"}>
+                                        Multiple Choice
+                                    </SelectItem>
+                                    <SelectItem key={index} value={"rating"}>
+                                        Rating
+                                    </SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                        {question.settings?.type === "text" ? <Input value={question.question} /> : ""}
+                    </CardContent>
+                    <CardFooter className="ml-10"></CardFooter>
+                </Card>
             })}
-
             <Button
                 className="max-w-screen-lg w-full"
                 onClick={() =>
